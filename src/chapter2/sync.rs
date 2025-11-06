a138fbb6380b4c99527a945a0974690fb69954b5
willie.chen <willie.chen@aceraeb.com>
2025-11-06 09:50:24 +0800

    微調

--- src/chapter2/sync.rs
+++ src/chapter2/sync.rs
@@ -34,16 +34,16 @@ pub fn sync(source: &str, target: &str) {
     }
 }
 
-fn read_paths_and_hashes(dir: &str) -> HashMap<String, PathBuf> {
-    let mut hashes = HashMap::new();
+fn read_paths_and_hashes(dir: &str) -> Vec<(PathBuf, String)> {
+    let mut fileList = Vec::new();
     for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_file() {
             let path = entry.path().to_path_buf();
             let hash = calculate_hash(&path);
-            hashes.insert(hash, path);
+            fileList.push((path, hash));
         }
     }
-    hashes
+    fileList
 }
 
 fn calculate_hash(path: &Path) -> String {
@@ -66,35 +66,44 @@ fn calculate_hash(path: &Path) -> String {
 }
 
 fn determine_actions(
-    source_hashes: &HashMap<String, PathBuf>,
-    target_hashes: &HashMap<String, PathBuf>,
+    source_hashes: &Vec<(PathBuf, String)>,
+    target_hashes: &Vec<(PathBuf, String)>,
     source: &str,
     target: &str,
 ) -> Vec<(&'static str, (Option<PathBuf>, Option<PathBuf>))> {
     let mut actions = Vec::new();
 
-    for (hash, path) in source_hashes {
-        // If the file does not exist in the target, copy it
-        if !target_hashes.contains_key(hash) {
+    for (path, hash) in source_hashes {
+        if !target_hashes
+            .iter()
+            .any(|(p, h)| p.file_name() == path.file_name())
+        {
+            // If the file does not exist in the target, copy it
             let sourcepath = Path::new(source).join(path.file_name().unwrap());
             let targetpath = Path::new(target).join(path.file_name().unwrap());
             actions.push(("copy", (Some(sourcepath), Some(targetpath))));
-        } else {
-            let target_path = target_hashes.get(hash).unwrap();
-            // If the file exists but in a different location, move it
-            if path.strip_prefix(source).unwrap() != target_path.strip_prefix(target).unwrap() {
-                let sourcepath = Path::new(target).join(target_path.file_name().unwrap());
-                let targetpath = Path::new(target).join(path.file_name().unwrap());
-                actions.push(("move", (Some(sourcepath), Some(targetpath))));
-            }
+        } else if target_hashes.iter().any(|(p, h)| h == hash) {
+            // If the file exists in the target but with a different name, move it
+            let target_path = target_hashes
+                .iter()
+                .find(|(p, h)| h == hash)
+                .unwrap()
+                .0
+                .clone();
+
+            let sourcepath = Path::new(target).join(target_path.file_name().unwrap());
+            let targetpath = Path::new(target).join(path.file_name().unwrap());
+            actions.push(("move", (Some(sourcepath), Some(targetpath))));
         }
     }
 
-    for (hash, path) in target_hashes {
+    for (path, hash) in target_hashes {
         // If the file does not exist in the source, delete it
-        if !source_hashes.contains_key(hash) {
-            let targetpath = Path::new(target).join(path.file_name().unwrap());
-            actions.push(("delete", (None, Some(targetpath))));
+        if !source_hashes
+            .iter()
+            .any(|(p, h)| p.file_name() == path.file_name())
+        {
+            actions.push(("delete", (None, Some(path.clone()))));
         }
     }
 
